d273434e10840f249c6ea0775e9b256524858857
may896868 <may896868@gmail.com>
2026-05-01 16:54:28 +0530

    datatypes

--- 09-datatypes.sh
+++ 09-datatypes.sh
@@ -7,4 +7,7 @@ SUM=$(($NUMBER1+$NUMBER2+$NAME))
 echo "sum is : $SUM"
 
 LEADERS=("MODI","PUTIN","TRUDO","TRUMP")
-echo "all the leaders are : $LEADERS"
+echo "all the leaders are : ${LEADERS}"
+echo "All the leaders are : ${LEADERS[@]}"
+echo "First leader is : ${LEADERS[0]}"
+echo " Second leader is : ${LEADERS[1]}"
