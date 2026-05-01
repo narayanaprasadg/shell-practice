cab778cb5d41acee2d16599e66075dabdf7eceab
may896868 <may896868@gmail.com>
2026-05-01 11:25:41 +0530

    arguments

---
+++ 04-arguments.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 :: welcome to alaska $PERSON2"
+echo "$PERSON2 :: Thank you $PERSON1, not finally zoom webinar"
+echo "$PERSON1 :: do we have a deal"
+echo "$PERSON2 :: yes you joins ukrain to russia"
