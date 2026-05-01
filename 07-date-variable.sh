d04ccfce4358807f5fcdb7141b30f460ba2aeff0
may896868 <may896868@gmail.com>
2026-05-01 15:19:24 +0530

    date-varibale

--- 07-date-variable.sh
+++ 07-date-variable.sh
@@ -4,9 +4,11 @@
 #echo " time stamp executed : $DATE"
 
 START_TIME=$(date +%s)
+echo "start-time is : $START_TIME"
 sleep 5
 
 END_TIME=$(date +%s)
+echo "end time is : $ END_TIME"
 
 TOTAL_TIME=$($END_TIME-$START_TIME)
 
