de905d2c3ecbc3c2852026acd7d5ac3e1e8b6a85
may896868 <may896868@gmail.com>
2026-05-01 15:37:44 +0530

    speacial-varibale

---
+++ 08-special-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo " All the variables passed to the script : $*"
+echo " All the variables passed to the script : $@"
+echo " script name : $0"
+echo " cirrent directory : $PWD"
+echo " who is running this : $ USER"
+echo " howm directory of user : $ HOME"
+echo " PID of the script : $$"
+sleep 30
+echo " PID of the last command in back ground is : $!"
+
