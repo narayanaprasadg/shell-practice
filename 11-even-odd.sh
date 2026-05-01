06c8329e8c7d454bcfa91218e3b7ceb917cb368f
may896868 <may896868@gmail.com>
2026-05-01 17:44:46 +0530

    conditions

---
+++ 11-even-odd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "please enter the number"
+read NUMBER
+if [ $(($NUMBER % 2)) -eq 0 ]; then 
+    echo "the given number $NUMBER" is even"
+else
+    echo " the given number $NUMBER is odd"
+fi
