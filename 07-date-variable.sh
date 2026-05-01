c171ed3e8151f524205a5ec22565158fcc6ad7ca
may896868 <may896868@gmail.com>
2026-05-01 15:11:34 +0530

    date-varibale

--- 07-date-variable.sh
+++ 07-date-variable.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-DATE=$(date)
-echo " time stamp executed : $DATE"
+#DATE=$(date)
+#echo " time stamp executed : $DATE"
+
+START_TIME=$(date +%s)
+sleep 5
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME::$($END_TIME-$START_TIME)
+
+echo "TOTAL_TIME_SCRIPT_EXECUTED : $TOTAL_TIME IN SECONDS"
